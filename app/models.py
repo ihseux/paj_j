42d34bfdd0f1bff19a4ab9ea63f643e1ab68051a
Loh Hao Yi <lohhaoyi@gmail.com>
2018-12-22 17:36:32 +0800

    added Profile message textfield

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(UserMixin, db.Model):
 
 	def avatar (self, size):
 		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-		return 'https://www,gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
